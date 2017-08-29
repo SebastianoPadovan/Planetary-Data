989b350095fb9a31c86d8f3714bf66668f84bc52
SebastianoPadovan <31437109+SebastianoPadovan@users.noreply.github.com>
2017-08-29 14:56:39 +0200

    Add files via upload

---
+++ GV.py
@@ -0,0 +1,17 @@
+class GV:
+   """ Collection of constants 
+       and useful quantities """
+   def __init__(self):
+      import numpy as np
+      self.G = 6.6742e-11
+      self.pi = np.pi
+      self.fp3G = 4.*self.pi*self.G/3.
+      self.sXd = 86400. 
+      self.sXy = self.sXd*365
+      self.sXMy= self.sXy*1e6
+      self.C2K = 273.15
+      self.SSa = 4.5e3 # [My] Solar system age
+      self.Rgc = 8.3144621 # [J/K/mol] Gas constant
+      self.d2r = np.pi/180.
+      self.r2d = 180./np.pi
+
